e5c6618edfa256123cb0adcb0e2b31f185252399
1xSh4dowx1 <duarte.afonso.rodrigues@gmail.com>
2025-04-08 13:24:36 +0100

    added the Serial Receiver files and his components, without testing it yet

---
+++ SLCDC/serial_receiver/receiver_controler/receiver_controler.vhd
@@ -0,0 +1,99 @@
+LIBRARY ieee; 
+USE ieee.std_logic_1164.all;
+
+entity receiver_controler is
+
+	port (
+		not_enRx	: 	in std_logic;
+		accept	: 	in std_logic;
+		pFlag    :  in std_logic;
+		dFlag    :  in std_logic;
+		RXerror  :  in std_logic;
+		
+		CLK		: 	in std_logic;
+		Reset 	:	in std_logic;		
+		
+		wr	 		: 	out std_logic;
+		init     :  out std_logic;
+		DXval    :  out std_logic
+	);
+end receiver_controler;
+
+architecture behavioral of receiver_controler is
+
+type STATE_TYPE is (State1, State2, State3, State4, State5, State6);
+
+signal CurrentState, NextState: STATE_TYPE;
+
+begin
+
+-- Flip-Flop's
+CurrentState <= State1 when RESET = '1' else NextState when rising_edge(clk);
+
+-- Generate Next State
+GenerateNextState:
+process (CurrentState, not_enRx, accept, pFlag, dFlag, RXerror)
+begin
+    case CurrentState is
+
+        when State1 =>
+            if not_enRx = '0' then
+                NextState <= State2;
+            else
+                NextState <= State1;
+            end if;
+
+        when State2 =>
+            if not_enRx = '1' then
+                NextState <= State1;
+            elsif dFlag = '1' then
+                NextState <= State3;
+            else
+                NextState <= State2;
+            end if;
+
+        when State3 =>
+            if not_enRx = '1' then
+                NextState <= State1;
+            elsif pFlag = '1' then
+                NextState <= State4;
+            else
+                NextState <= State3;
+            end if;
+
+        when State4 =>
+            if not_enRx = '1' then
+                NextState <= State1;
+            elsif RXerror = '1' then
+                NextState <= State5;
+            else
+                NextState <= State4;
+            end if;
+
+        when State5 =>
+            if not_enRx = '1' then
+                NextState <= State1;
+            elsif accept = '1' then
+                NextState <= State6;
+            else
+                NextState <= State5;
+            end if;
+
+        when State6 =>
+            if not_enRx = '1' or accept = '0' then
+                NextState <= State1;
+            else
+                NextState <= State6;
+            end if;
+
+    end case;
+end process;
+
+-- Generate outputs
+
+
+wr	 	<= '1' when (CurrentState = State2) else '0';
+init 	<= '1' when (CurrentState = State1) else '0';
+DXval <= '1' when (CurrentState = State5) else '0';
+
+end behavioral;
---
+++ SLCDC/serial_receiver/serial_receiver.vhd
@@ -0,0 +1,132 @@
+LIBRARY ieee; 
+USE ieee.std_logic_1164.all;
+
+entity serial_receiver is
+
+	port (
+		SS			: 	in std_logic;
+		SCLK		: 	in std_logic;
+		SDX		: 	in std_logic;
+		Reset 	:	in std_logic;	
+		accept   :  in std_logic;
+		
+		DXval 	: 	out std_logic;
+		Data		: 	out std_logic_vector(4 downto 0)			
+	);
+end serial_receiver;
+
+architecture structural of serial_receiver is
+
+	component counter_3bits is
+		port (
+			CE     : in std_logic;
+			CLK    : in std_logic;
+			reset  : in std_logic;
+			count  : out std_logic_vector(2 downto 0)
+		);
+	end component;
+
+	component parity_checker is
+		port (
+			data   : in std_logic;
+			init   : in std_logic;
+			CLK    : in std_logic;
+			Reset  : in std_logic;
+			error  : out std_logic
+		);
+	end component;
+
+	component ShiftRegisterSerialReceiver is
+		port (
+			reset : in std_logic;
+			data  : in std_logic;
+			SCLK  : in std_logic;
+			E     : in std_logic;
+			D     : out std_logic_vector(4 downto 0)
+		);
+	end component;
+
+	component receiver_controler is 
+		port (
+			not_enRx : in std_logic;
+			accept   : in std_logic;
+			pFlag    : in std_logic;
+			dFlag    : in std_logic;
+			RXerror  : in std_logic;
+			CLK      : in std_logic;
+			Reset    : in std_logic;
+			wr       : out std_logic;
+			init     : out std_logic;
+			DXval    : out std_logic
+		);
+	end component;
+
+
+	signal wr_sig 			: std_logic;
+	signal init_sig 		: std_logic;
+	signal dFlag_sig 		: std_logic;
+	signal pFlag_sig 		: std_logic;
+	signal RXerror_sig	: std_logic;
+	signal eq5 				: std_logic;
+	signal eq6 				: std_logic;
+	signal counter_val	: std_logic_vector(2 downto 0);
+	signal data_shift 	: std_logic_vector(4 downto 0);
+
+begin
+
+	-- Shift Register
+	ShiftReg: ShiftRegisterSerialReceiver
+		port map (
+			reset  => Reset,
+			data   => SDX,
+			SCLK   => SCLK,
+			E      => wr_sig,
+			D      => data_shift
+		);
+
+	-- Parity Checker
+	ParityCheck: parity_checker
+		port map (
+			data   => SDX,
+			init   => init_sig,
+			CLK    => SCLK,
+			Reset  => Reset,
+			error  => RXerror_sig
+		);
+
+	-- Counter
+	Counter3bit: counter_3bits
+		port map (
+			CE     => wr_sig,
+			CLK    => SCLK,
+			reset  => Reset,
+			count  => counter_val
+		);
+
+	-- Comparadores
+	eq5 <= '1' when counter_val = "101" else '0';
+	eq6 <= '1' when counter_val = "110" else '0';
+
+	-- Flags
+	pFlag_sig <= eq5;
+	dFlag_sig <= eq6;
+
+	-- Controle
+	Controller: receiver_controler
+		port map (
+			not_enRx => SS,
+			accept   => accept,
+			pFlag    => pFlag_sig,
+			dFlag    => dFlag_sig,
+			RXerror  => RXerror_sig,
+			CLK      => SCLK,
+			Reset    => Reset,
+			wr       => wr_sig,
+			init     => init_sig,
+			DXval    => DXval
+		);
+
+	-- Saída
+	Data(4 downto 0) <= data_shift;
+
+end structural;
